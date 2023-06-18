51eb290ba84921b561d5a115017b7802bc20613d
Oz.ETH <48379417+Ozurumba@users.noreply.github.com>
2023-06-18 21:09:21 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,16 @@ export default function Home() {
   return (
     <>
       <Head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7EMKTRW285"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7EMKTRW285');
+</script>
+  
         <title>Home - JGFunded</title>
 
         <meta
